eff3d27ff8315b79e9dc3df72f42b8f8552ae7d7
Waterstar1 <waterstar12001@gmail.com>
2020-06-03 19:42:23 -0700

    Button adds reviews, need to change that it is called only once at refresh

--- src/content.js
+++ src/content.js
@@ -31,13 +31,12 @@ function addInfo() {
             name = name.trim().split(" ");
             var firstName = name[0];
             var lastName = name[name.length - 1];
-            var review = lookUpInstructor(firstName, lastName);
-            editPage(review, item)
+            lookUpInstructor(firstName, lastName, item);
         }
     }
 }
 
-function lookUpInstructor(firstName, lastName) {
+function lookUpInstructor(firstName, lastName, element) {
     const rmpAPI = "https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q="+ firstName + "+" + lastName +"&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+teacherfullname_autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=score+desc&defType=edismax&siteName=rmp&rows=20&group=off&group.field=content_type_s&group.limit=20&fq=schoolid_s%3A1072"
     const request = $.ajax({url: rmpAPI, success: function(data) {
         console.log("Capture data success!")
@@ -47,35 +46,31 @@ function lookUpInstructor(firstName, lastName) {
         data = data.substring(5, data.length - 1);     
 
         var dataArr = JSON.parse(data).response.docs[0];
-        console.log(dataArr);
         if (dataArr !== undefined) {
-            var OR = (Array.from(dataArr).some(item => _.isEqual(item, "averageratingscore_rf"))) ? dataArr.averageratingscore_rf : "--";
-            var LoD = (Array.from(dataArr).some(item => _.isEqual(item, "averageeasyscore_rf"))) ? dataArr.averageeasyscore_rf : "--";
-            var NoR = (Array.from(dataArr).some(item => _.isEqual(item, "total_number_of_ratings_i"))) ? dataArr.total_number_of_ratings_i : "--";
+            var OR = (dataArr.hasOwnProperty("averageratingscore_rf")) ? dataArr.averageratingscore_rf : "--";
+            var LoD = (dataArr.hasOwnProperty("averageeasyscore_rf")) ? dataArr.averageeasyscore_rf : "--";
+            var NoR = (dataArr.hasOwnProperty("total_number_of_ratings_i")) ? dataArr.total_number_of_ratings_i : "--";
         } else {
             var OR = "--";
             var LoD = "--";
             var NoR = "--";
         }
+        editPage(OR, LoD, NoR, element);
 
-        return [OR, LoD, NoR];
-        
     })
         .fail(function (Response) {
-          //do something when any error occurs.
     });;
 }
 
-function editPage(review, item) {
+function editPage(OR, LoD, NoR, item) {
     var additional = item;
-    var hi = "-";
     $($(additional).children("span")).wrapAll("<div ></div>");
     $(additional).append("<span class=ls-label>&#9733</span>");
-    $(additional).append("<span>" + review[0] + "</span>");
+    $(additional).append("<span>" + OR + "</span>");
     $(additional).append("<span class=ls-label>&nbsp LoD:</span>");
-    $(additional).append("<span>" + hi + "</span>");
+    $(additional).append("<span>" + LoD + "</span>");
     $(additional).append("<span class=ls-label>&nbsp #</span>");
-    $(additional).append("<span>" + hi + "</span>");
+    $(additional).append("<span>" + NoR + "</span>");
 }
 
 
