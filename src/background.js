5c9708cc94c432bc09fccc828c99a847f801bbcf
Waterstar1 <waterstar12001@gmail.com>
2020-06-03 21:16:56 -0700

    Fixed final display on webpage

--- src/background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(tellContentScript);
-console.log("random");
-function tellContentScript(tab) {
-    let msg = {text: "send"}
-    chrome.tabs.sendMessage(tab.id, msg);
-}
--- src/content.js
+++ src/content.js
@@ -1,37 +1,15 @@
 
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message, sender, sendResponse) {
-    if (message.text == "send") {
-        console.log("mochi");
-        addInfo();
-    }
-}
-
-function addReviews() {
-    var list = document.querySelectorAll('[aria-label="Instructors"]');
-    console.log(list[0]);
-
-    var changes = document.getElementsByClassName("ls-instructors fspmedium")
-    var additional =  changes[0];
-    $($(additional).children("span")).wrapAll("<div ></div>");
-    $(additional).append("<span class=ls-label>&#9733</span>");
-    var wf = 4.5
-    $(additional).append("<span>" + wf + "</span>");
-    $(additional).append("<span class=ls-label>&nbsp LoD:</span>");
-    $(additional).append("<span>3</span>");
-    $(additional).append("<span class=ls-label>&nbsp #</span>");
-    $(additional).append("<span>56</span>");
-}
-
+addInfo();
 function addInfo() {
     var elements = document.getElementsByClassName("ls-instructors fspmedium");
+    var round = 0;
     for (let item of elements) {
         var instructors = $(item).text().trim().replace(new RegExp("\\s+", "g"), " ").split(",");    
         for(let name of instructors) {
             name = name.trim().split(" ");
             var firstName = name[0];
             var lastName = name[name.length - 1];
-            lookUpInstructor(firstName, lastName, item);
+            lookUpInstructor(firstName, lastName, item, round);
         }
     }
 }
@@ -64,7 +42,9 @@ function lookUpInstructor(firstName, lastName, element) {
 
 function editPage(OR, LoD, NoR, item) {
     var additional = item;
-    $($(additional).children("span")).wrapAll("<div ></div>");
+    $(additional).append("<div></div>");
+    children = $(additional).children("div");
+    additional = children[children.length - 1];
     $(additional).append("<span class=ls-label>&#9733</span>");
     $(additional).append("<span>" + OR + "</span>");
     $(additional).append("<span class=ls-label>&nbsp LoD:</span>");
@@ -72,41 +52,3 @@ function editPage(OR, LoD, NoR, item) {
     $(additional).append("<span class=ls-label>&nbsp #</span>");
     $(additional).append("<span>" + NoR + "</span>");
 }
-
-
-function logWord() {
-    let selectedText = window.getSelection().toString();
-    console.log(selectedText);
-
-    var firstName = "josh"
-    var lastName = "hug"
-    
-
-    const rmpAPI = "https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q="+ firstName + "+" + lastName +"&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+teacherfullname_autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=score+desc&defType=edismax&siteName=rmp&rows=20&group=off&group.field=content_type_s&group.limit=20&fq=schoolid_s%3A1072"
-    const request = $.ajax({url: rmpAPI, success: function(data) {
-        console.log("Capture data success!")
-    }}).done(function (response) {
-        console.log(response);    
-        var data = response.toString();
-        data = data.substring(5, data.length - 1);     
-
-        var dataArr = JSON.parse(data).response.docs[0];
-        
-        if (response !== undefined) {
-            var OR = dataArr.averageratingscore_rf;
-            var LoD = dataArr.averageeasyscore_rf;
-            var NoR = dataArr.total_number_of_ratings_i;
-
-            console.log(OR, LoD, NoR);
-        }
-
-        
-    })
-        .fail(function (Response) {
-          //do something when any error occurs.
-    });;
-}
-
-
-
-
