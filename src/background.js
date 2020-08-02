34da98e22b49f4b3390e045912e2fcfc9d75a434
Waterstar1 <waterstar12001@gmail.com>
2020-08-01 17:15:46 -0700

    Added button for grades

--- src/background.js
+++ src/background.js
@@ -4,26 +4,3 @@ function tellContentScript(tab) {
     let msg = {text: "send it, dude"}
     chrome.tabs.sendMessage(tab.id, msg)
 }
-
-function testAPI() {
-    console.log("hi");
-    btAPI = "https://www.berkeleytime.com/api/grades/grades_json/?form=long";
-    const request = $.ajax({url: btAPI, success: function(data) {
-        console.log("Capture data success!")
-    }}).done(function (response) {
-        console.log(response); 
-        
-        var catalog = response.courses;
-        console.log(catalog);
-        console.log(catalog[0].abbreviation);
-        
-        const toCourseIDS = new Map();
-        for(key in catalog) {
-            value = catalog[key];
-            toCourseIDS.set(value.title.toLowerCase().replace(new RegExp("\\s+", "g"), ""), value.id);
-        }
-        console.log(toCourseIDS);
-    })
-        .fail(function (Response) {
-    });;
-}
--- src/content.js
+++ src/content.js
@@ -2,70 +2,67 @@ chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse){
     if (message.text == "send it, dude") {
-        popUp()
+        getCatalog();
     }
 }
 
-function testAPI() {
-    console.log("hi");
+function getCatalog() {
     btAPI = "https://www.berkeleytime.com/api/grades/grades_json/?form=long";
-    const request = $.ajax({url: btAPI, success: function(data) {
-        console.log("Capture data success!")
-    }}).done(function (response) {
-        console.log(response); 
+    const request = $.ajax({url: btAPI}).done(function (response) {
         
         var catalog = response.courses;
-        console.log(catalog);
-        console.log(catalog[0].abbreviation);
-        
-        const toCourseIDS = new Map();
+        toCourseIDS = new Map();
         for(key in catalog) {
             value = catalog[key];
             toCourseIDS.set(value.title.toLowerCase().replace(new RegExp("\\s+", "g"), ""), value.id);
         }
-        console.log(toCourseIDS);
+        addInfo();
     })
         .fail(function (Response) {
     });;
 }
 
-function popUp() {
-    var elements = document.getElementsByClassName("ls-course-title fmpbook");
-    $(elements[0]).append("<div id='popup' style='display: none'>description text here</div>");
-    
-}
-
-
-//addInfo();
 function addInfo() {
     getInstructors();
 }
 
 /* Finds instructor names from Cal Schedule Website */
 function getInstructors() {
+
     var elements = document.getElementsByClassName("ls-instructors fspmedium");
+
     for (let item of elements) {
-        var instructors = $(item).text().trim().replace(new RegExp("\\s+", "g"), " ").split(",");    
+
+        // Finds course title for respectively respective professors
+        var courseTitle = item.parentNode.querySelector('.ls-course-title').innerHTML;
+        var simpleCourseTitle = courseTitle.toLowerCase().replace(new RegExp("\\s+", "g"), "")
+
+        // Parses instructor description into multiple professors if possible 
+        var instructors = $(item).text().trim().replace(new RegExp("\\s+", "g"), " ").split(",");
+        
         for(let name of instructors) {
+            
+            // Parses professor name into first and last. 
             name = name.trim().split(" ");
             var firstName = name[0];
             var lastName = name[name.length - 1];
-            $(item).append("<div></div>");
-            children = $(item).children("div");
-            additional = children[children.length - 1];
 
-            lookUpInstructor(firstName, lastName, additional);
+            // New section for ratings    
+            $(item).append("<div></div>");
+            var children = $(item).children("div");
+            var additional = children[children.length - 1];
+
+            lookUpInstructor(firstName, lastName, additional, simpleCourseTitle);
         }
     }
 }
 
 /* Retrieves instructor data from RateMyProfessor API */
-function lookUpInstructor(firstName, lastName, element) {
+function lookUpInstructor(firstName, lastName, element, simpleCourseTitle) {
     const rmpAPI = "https://solr-aws-elb-production.ratemyprofessors.com//solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q="+ firstName + "+" + lastName +"&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+teacherfullname_autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=score+desc&defType=edismax&siteName=rmp&rows=20&group=off&group.field=content_type_s&group.limit=20&fq=schoolid_s%3A1072"
-    const request = $.ajax({url: rmpAPI, success: function(data) {
-        console.log("Capture data success!")
-    }}).done(function (response) {
-        console.log(response);    
+    const request = $.ajax({url: rmpAPI}).done(function (response) {  
+
+        // Parse json response from Rate My Professor into particular values
         var data = response.toString();
         data = data.substring(5, data.length - 1);     
 
@@ -79,19 +76,46 @@ function lookUpInstructor(firstName, lastName, element) {
             var LoD = "--";
             var NoR = "--";
         }
-        editPage(OR, LoD, NoR, element);
 
+        editPage(OR, LoD, NoR, element, simpleCourseTitle);
     })
         .fail(function (Response) {
     });;
 }
 
 /* Adds RateMyProfessor data to Berkeley schedule website */
-function editPage(OR, LoD, NoR, additional) {
+function editPage(OR, LoD, NoR, additional, simpleCourseTitle) {
+
+    // Rate My Professor additions
     $(additional).append("<span class=ls-label>&#9733</span>");
     $(additional).append("<span>" + OR + "</span>");
     $(additional).append("<span class=ls-label>&nbsp LoD:</span>");
     $(additional).append("<span>" + LoD + "</span>");
     $(additional).append("<span class=ls-label>&nbsp #</span>");
     $(additional).append("<span>" + NoR + "</span>");
+
+    //Add grade distribution button
+    var button = document.createElement("a");
+    button.className = "grade-button";
+    button.innerText = "Button!"
+
+    additional.appendChild(button);
+
+    requestGrades(simpleCourseTitle)
+
+}
+
+function requestGrades() {
+    btAPI = "https://www.berkeleytime.com/api/grades/grades_json/?form=long";
+}
+ 
+
+
+
+
+
+function popUp() {
+    var elements = document.getElementsByClassName("ls-course-title fmpbook");
+    $(elements[0]).append("<div id='popup' style='display: none'>description text here</div>");
+    
 }
