541a63a3724079abaf221b3f6e2ece86aeca6585
Waterstar1 <waterstar12001@gmail.com>
2020-08-01 23:48:50 -0700

    Finished popup

--- src/content.js
+++ src/content.js
@@ -9,7 +9,6 @@ function gotMessage(message, sender, sendResponse){
 function getCatalog() {
     btAPI = "https://www.berkeleytime.com/api/grades/grades_json/?form=long";
     const request = $.ajax({url: btAPI}).done(function (response) {
-        
         var catalog = response.courses;
         toCourseIDS = new Map();
         for(key in catalog) {
@@ -107,11 +106,11 @@ function editPage(OR, LoD, NoR, additional, firstName, lastName, simpleCourseTit
     button.setAttribute("instructor", parsedName.toUpperCase());
 
     // Handles popup events due to mouse hovering over the button
-    button.addEventListener("mouseover", showInfo(button, additional));
+    button.addEventListener("mouseover", showInfo(button, additional, firstName, lastName));
     button.addEventListener("mouseout", removeInfo(button, additional));
 }
 
-function showInfo(button, parent) {
+function showInfo(button, parent, firstName, lastName) {
     return function() {
         var containerCheck = parent.querySelector(".grade-container");
 
@@ -122,7 +121,7 @@ function showInfo(button, parent) {
 
                 var gradeIDArr = []; 
                 console.log(response.length);
-                for (i = 0; i < response.length; i++) {
+                for (var i = 0; i < response.length; i++) {
                     var instructor = response[i].instructor;
                     if (instructor == button.getAttribute("instructor")) {
                         gradeIDArr.push(response[i].grade_id);
@@ -137,16 +136,15 @@ function showInfo(button, parent) {
                 popup.className = "grade-popup";
 
 
-                popup.innerHTML = "hello";
+                popup.innerHTML = firstName + " " + lastName;
                 var graph = gradeDistribution(gradeDataURL);
                 popup.appendChild(graph);
 
                 container.appendChild(popup);
                 parent.append(container);
-                    
-
-
-                //container.className = "grade-container show ";
+                container.className = "grade-container show"      
+                
+                console.log(popup);
 
             })
                 .fail(function (Response) {
@@ -160,22 +158,59 @@ function showInfo(button, parent) {
 
 
 function gradeDistribution(gradeDataURL) {
-    var graph = document.createElement('table');
-    graph.className = "grades-dist-table";
+    var graph = document.createElement('canvas');
+    graph.width = "1200";
+    graph.height = "800";
+    var grades = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D', 'F', 'P', 'NP'];
+    var values = [];
 
     const request = $.ajax({url: gradeDataURL}).done(function (response) {  
-        console.log(response);
-
+    
+        for (letter of grades) {
+            values.push(response[letter].percent);
+        } 
+        console.log(values);
+        editInfo(graph, grades, values);
+    
     })
         .fail(function (Response) {
+            values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            editInfo(graph, grades, values);
     });;
     return graph;
 
 }
 
+function editInfo(graph, grades, values) {
+    var ctx = graph.getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: grades,
+            datasets: [{
+                fillColor: "#79D1CF",
+                strokeColor: "#79D1CF",
+                data: values
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
 function removeInfo(button, parent) {
     return function(){
-        parent.querySelector(".grade-container").className="grade-container hide";
+        var container = parent.querySelector(".grade-container")
+        if (container) {
+            container.className="grade-container hide";
+        }   
       }
 }
 
